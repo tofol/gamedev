8dc63f3ce311176430a60fd33c91a17f54c58c11
Tobal <lopeztobal@gmail.com>
2016-08-01 11:47:08 +0200

    Update main.py
    Adding more things

--- tutorials/tilemap/part 02/main.py
+++ tutorials/tilemap/part 02/main.py
@@ -3,18 +3,23 @@
 # Collisions and Tilemaps
 # Video link: https://youtu.be/ajR4BZBKTr4
 import pygame as pg
+import pyglet.clock
 import sys
-from os import path
+from os import environ, path
 from settings import *
 from sprites import *
 
-class Game:
+
+class Game(object):
     def __init__(self):
+        super(Game, self).__init__()
+        environ['SDL_VIDEO_CENTERED'] = '1'
         pg.init()
         self.screen = pg.display.set_mode((WIDTH, HEIGHT))
         pg.display.set_caption(TITLE)
-        self.clock = pg.time.Clock()
-        pg.key.set_repeat(500, 100)
+        self.clock = pyglet.clock.Clock()
+        self.clock.set_fps_limit(FPS)
+        # pg.key.set_repeat(500, 100)
         self.load_data()
 
     def load_data(self):
@@ -25,7 +30,6 @@ class Game:
                 self.map_data.append(line)
 
     def new(self):
-        # initialize all variables and do all the setup for a new game
         self.all_sprites = pg.sprite.Group()
         self.walls = pg.sprite.Group()
         for row, tiles in enumerate(self.map_data):
@@ -36,10 +40,11 @@ class Game:
                     self.player = Player(self, col, row)
 
     def run(self):
-        # game loop - set self.playing = False to end the game
         self.playing = True
         while self.playing:
-            self.dt = self.clock.tick(FPS) / 1000
+            # self.dt = self.clock.tick(FPS) / 1000
+            pg.display.set_caption(TITLE + str(self.clock.get_fps()) + ' FPS')
+            self.clock.tick()
             self.events()
             self.update()
             self.draw()
@@ -49,14 +54,13 @@ class Game:
         sys.exit()
 
     def update(self):
-        # update portion of the game loop
         self.all_sprites.update()
 
     def draw_grid(self):
         for x in range(0, WIDTH, TILESIZE):
             pg.draw.line(self.screen, LIGHTGREY, (x, 0), (x, HEIGHT))
-        for y in range(0, HEIGHT, TILESIZE):
-            pg.draw.line(self.screen, LIGHTGREY, (0, y), (WIDTH, y))
+            for y in range(0, HEIGHT, TILESIZE):
+                pg.draw.line(self.screen, LIGHTGREY, (0, y), (WIDTH, y))
 
     def draw(self):
         self.screen.fill(BGCOLOR)
@@ -65,13 +69,12 @@ class Game:
         pg.display.flip()
 
     def events(self):
-        # catch all events here
         for event in pg.event.get():
             if event.type == pg.QUIT:
-                self.quit()
+                self.playing = False
             if event.type == pg.KEYDOWN:
                 if event.key == pg.K_ESCAPE:
-                    self.quit()
+                    self.playing = False
                 if event.key == pg.K_LEFT:
                     self.player.move(dx=-1)
                 if event.key == pg.K_RIGHT:
@@ -80,6 +83,15 @@ class Game:
                     self.player.move(dy=-1)
                 if event.key == pg.K_DOWN:
                     self.player.move(dy=1)
+            elif event.type == pg.KEYUP:
+                if event.key == pg.K_LEFT:
+                    self.player.move(dx=0)
+                if event.key == pg.K_RIGHT:
+                    self.player.move(dx=0)
+                if event.key == pg.K_UP:
+                    self.player.move(dy=0)
+                if event.key == pg.K_DOWN:
+                    self.player.move(dy=0)
 
     def show_start_screen(self):
         pass
@@ -87,10 +99,15 @@ class Game:
     def show_go_screen(self):
         pass
 
-# create the game object
-g = Game()
-g.show_start_screen()
-while True:
-    g.new()
-    g.run()
-    g.show_go_screen()
+
+def main():
+    g = Game()
+    g.show_start_screen()
+    while True:
+        g.new()
+        g.run()
+        g.show_go_screen()
+        g.quit()
+
+if __name__ == '__main__':
+    main()
